674fdbfe495a804d9a11875a2bd61b51c23391c1
haileygarrison <133424266+haileygarrison@users.noreply.github.com>
2023-09-18 18:25:55 -0700

    1

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "d209df2ccf18d8f7e5edca392b0bf43a"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "dca2e2d1f54347d7b44b02686d422b00"
